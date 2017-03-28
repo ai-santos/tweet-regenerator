f5479b0a6591d8a9d6ee7bc7c0c05a0ada803026
Aileen <asantos3026@gmail.com>
2017-03-28 13:52:17 -0700

    WIP setting up twitter app

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,1 +1,8 @@
 import express from 'express'
+
+const twitterAPI = require('node-twitter-api');
+const twitter = new twitterAPI({
+let consumerKey: 'your consumer Key',
+let consumerSecret: 'your consumer secret',
+    callback: 'http://yoururl.tld/something'
+});
