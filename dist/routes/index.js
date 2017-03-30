754d1e28e80486cc9e46bc2d1a7b44aec0badf38
Aileen <asantos3026@gmail.com>
2017-03-30 15:19:22 -0700

    WIP tweets are now being added to the database

--- dist/routes/index.js
+++ dist/routes/index.js
@@ -16,6 +16,10 @@ var _config = require('../../config.js');
 
 var _config2 = _interopRequireDefault(_config);
 
+var _database = require('../database/database.js');
+
+var _database2 = _interopRequireDefault(_database);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var router = _express2.default.Router();
@@ -23,15 +27,18 @@ var router = _express2.default.Router();
 var twitter = new _twitter2.default(_config2.default);
 
 var params = {
-  screen_name: 'asantos3026',
   q: 'asantos3026',
-  count: 10
+  lang: 'en',
+  result_type: 'recent'
 };
 
 router.get('/', function (request, response) {
   twitter.get('search/tweets', params, function (error, tweets, twitterResponse) {
-    response.json(tweets);
-    // console.log('my tweets', tweets)
+    for (var i = 0; i < tweets.statuses.length; i++) {
+      console.log('my tweet', tweets.statuses[i].text);
+      _database2.default.addTweet(tweets.statuses[i].text);
+    }
+    response.json(tweets.statuses[0].text);
   });
 });
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,23 +1,28 @@
 import express from 'express'
 import twitterAPI from 'twitter'
 import config from '../../config.js'
+import db from '../database/database.js'
 
 const router = express.Router()
 
 const twitter = new twitterAPI(config)
 
 const params = {
-  screen_name: 'asantos3026',
   q: 'asantos3026',
-  count: 10
+  lang: 'en',
+  result_type: 'recent'
 }
 
 router.get('/', (request, response) => {
   twitter.get('search/tweets', params, (error, tweets, twitterResponse) => {
-    response.json(tweets)
-    // console.log('my tweets', tweets)
+    for(var i = 0; i < tweets.statuses.length; i++) {
+      console.log('my tweet', tweets.statuses[i].text)
+      db.addTweet(tweets.statuses[i].text)
+    }
+    response.json(tweets.statuses[0].text)
   })
 })
+
 
 
 export default router
